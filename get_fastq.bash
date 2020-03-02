0aa9ab147a625861249652155a1dbc64d8a23f65
Cory Dubois <cdubois@wilkcomp01.ccbb.utexas.edu>
2020-03-02 14:36:46 -0600

    added centroid and matchtable files

--- get_fastq.bash
+++ get_fastq.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Use -f folder to specify organism of interest
+## Use -f folder to specify organism of interest
 while getopts ":f:" opt; do
   case $opt in
     f) folder="$OPTARG"
@@ -11,7 +11,7 @@ while getopts ":f:" opt; do
 done
 
 
-filename=$folder/SraAccList.txt
+filename=$folder/Sra_real.txt
 mkdir $folder/fastq
 
 # Loop through all SRA IDs
@@ -19,18 +19,38 @@ while read -r line;
 do
 	
 	name="$line"
-	echo $name
 	# See if SRA ID is for a nanopore read
 	file_count=$(grep $name $folder/nanopore.txt)
 	size=${#file_count} 
 	
-	# If ID isn't nanopore, use fasterq-dump to get fastq files
-	if [[ $size -eq 0 ]]; then
-		echo $fnew
-		fasterq-dump $name -O $folder/fastq -e 8
-	# If ID is nanopore, use fastq-dump to get fastq files
-	else
-		fastq-dump --table SEQUENCE $name -O $folder/fastq
+	exists=$(ls $folder/fastq | grep $name)
+	exists_size=${#exists}
+	
+	testing=$(echo "$exists" | grep "_1")
+        testing1=${#testing}
+
+	t=$(echo "$exists" | grep "_2")
+	t2=${#t}
+
+	if [[ $exists_size -eq 0 ]]; then
+
+                # If ID isn't nanopore, use fasterq-dump to get fastq files
+                if [[ $size -eq 0 ]]; then
+                        echo "Fasterq $name"
+                        #fasterq-dump $name -O $folder/fastq -e 8 -f &
+			#prefetch $name
+			#fastq-dump --split-files $name -O $folder/fastq --skip-technical
+                # If ID is nanopore, use fastq-dump to get fastq files
+                else
+                        echo "WEIRDOOOOO $name"
+                        #fastq-dump --table SEQUENCE $name -O $folder/fastq
+                fi
+#	elif [[ $testing1 -ne 0 ]] && [[ $t2 -eq 0 ]]; then
+#		testing=$(echo "$exists" | grep "_1")
+#		testing1=${#testing}
+#		echo $testing
+#		echo $name
 	fi
-# This feads whatever
+
+# This feeds the appropriate file into the while loop
 done < "$filename"
